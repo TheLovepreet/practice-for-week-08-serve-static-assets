eab1a71927717aeeb16d5a1183fd79b1319147e0
TheLove <lovepreetsinghcad@gmail.com>
2023-01-16 14:35:36 -0500

    code for sending static asset (HTMl page) in response in HTTP

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Your code here
+  const htmlPage = fs.readFileSync('./index.html','utf-8');
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html')
+  res.end(htmlPage);
 });
 
 const port = 5000;
